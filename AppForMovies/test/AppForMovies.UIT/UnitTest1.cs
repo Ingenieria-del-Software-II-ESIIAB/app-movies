f4d8dc12183eeb4f6a7986f5f5ebd423d2505f01
UCLM\Elena.Navarro <elena.navarro@uclm.es>
2022-09-20 18:09:33 +0200

    Create Repo Structure

---
+++ AppForMovies/test/AppForMovies.UIT/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AppForMovies.UIT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AppForMovies/test/AppForMovies.UT/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AppForMovies.UT
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
